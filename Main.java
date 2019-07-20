2af1a381b1af1d4a62793e7b4b00119558f5ce84
medidiparthiv <53120768+medidiparthiv@users.noreply.github.com>
2019-07-20 11:10:20 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+	Scanner x = new Scanner(System.in);
+	    System.out.print("Please Enter Your Name : ");
+	    String y = x.nextLine();
+		System.out.println("You Entered Your Name As : " + y);
+	}
+}
